d9608621224adfd06b4ba242b880ee52ecc64fa0
dahyunhub <tls848@naver.com>
2026-03-08 15:44:14 +0900

    jpa dependency

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,20 +2,24 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public void home() {
-        System.out.println("Home page requested");
-        this.contact();
+    @ResponseBody
+    public int home() {
+        System.out.println("home");
+        return 22;
     }
 
     @GetMapping("/about")
-    public void about() {
+    @ResponseBody
+    public String about() {
         System.out.println("about");
-        this.contact();
+        return "안녕하세요.";
+
     }
 
     public void contact() {
